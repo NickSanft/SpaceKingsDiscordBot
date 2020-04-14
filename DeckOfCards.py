4c9f99d1c4678d90471435dc9876e2c9e36d4c6d
Nick <nsanft@sanft.com>
2020-04-14 16:02:55 -0500

    More refactoring, removed CardUtils.py

--- DeckOfCards.py
+++ DeckOfCards.py
@@ -1,26 +1,44 @@
-import CardUtils
 import random
+import re
+
+regularSuits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+ranks = ["Ace", "2", "3", "4", "5", "6", "7",
+         "8", "9", "10", "Jack", "Queen", "King"]
+successRanks = ['Ace', 'Jack', 'Queen', 'King']
+
 
 class Deck:
 
     def __init__(self):
-        self.cards = [ Card( rank, suit, rank + " of " + suit) for rank in CardUtils.getRanks() for suit in CardUtils.getSuits()]
-        self.cards.append(Card("Joker","Red","Red Joker"))
-        self.cards.append(Card("Joker","Black","Black Joker"))
+        self.cards = [Card(rank, suit, rank + " of " + suit)
+                      for rank in ranks for suit in regularSuits]
+        self.cards.append(Card("Joker", "Red", "Red Joker"))
+        self.cards.append(Card("Joker", "Black", "Black Joker"))
 
         random.shuffle(self.cards)
-    
+
     def drawCard(self):
         return self.cards.pop()
-        
 
-        
 
 class Card:
     def __init__(self, rank, suit, description):
         self.suit = suit
         self.rank = rank
         self.description = description
+
+    def isSuccess(self):
+
+        if any(re.findall('|'.join(successRanks), self.rank)):
+            return True
+        return False
+
+    def isFailure(self):
+        return self.rank == "Joker"
+
+    def isQueenOfHearts(self):
+        return self.suit == "Hearts" and self.rank == "Queen"
+
 
 if __name__ == "__main__":
     deck = Deck()
--- SpaceKingsBot.py
+++ SpaceKingsBot.py
@@ -3,7 +3,6 @@ import sys
 import inspect
 import random
 import time
-import CardUtils
 from DeckOfCards import Deck
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -19,9 +18,10 @@ help_attrs = dict(hidden=True)
 bot = Bot(command_prefix='/', help_attrs=help_attrs)
 decksByUser = {}
 
-failureMessages = ["It couldn't be too bad, could it?", "Maybe you'll just slip on a banana peel."]
-criticalfailureMessages = ["Pray to your gods.", "Aw man, am I gonna die?", "This is funny in a cosmic sort of way."]
-
+failureMessages = ["It couldn't be too bad, could it?",
+                   "Maybe you'll just slip on a banana peel."]
+criticalfailureMessages = ["Pray to your gods.",
+                           "Aw man, am I gonna die?", "This is funny in a cosmic sort of way."]
 
 
 """
@@ -40,6 +40,7 @@ async def on_ready():
 @bot.event
 async def on_read():
     print("bot logged in")
+
 
 async def initDeck(user: str):
     decksByUser[user] = Deck()
@@ -60,16 +61,15 @@ async def draw(ctx, numCards: int):
     queenOfHearts = False
 
     await ctx.send("Drawing " + str(numCards) + " for " + author.name + "...")
+    time.sleep(1)
 
     for i in range(0, numCards):
         card = decksByUser[user].drawCard()
-        print(card.suit)
-        print(card.rank)
-        if CardUtils.isSuccess(card.rank):
+        if card.isSuccess():
             numSuccesses += 1
-        if CardUtils.isFailure(card.rank):
+        if card.isFailure():
             numFailures += 1
-        if CardUtils.isQueenOfHearts(card.suit, card.rank):
+        if card.isQueenOfHearts():
             queenOfHearts = True
 
         await ctx.send(author.name + " drew: " + card.description + ". Cards left: " + str(len(decksByUser[user].cards)))
@@ -79,19 +79,19 @@ async def draw(ctx, numCards: int):
             await ctx.send("Out of cards! getting a new deck...")
             time.sleep(1)
             await initDeck(user)
-    
+
     resultMessage = "```Total number of Successes: " + str(numSuccesses) + "\n"
 
     if(queenOfHearts):
         resultMessage += "Queen Of Hearts! Add your charm to the number of successes! \n"
-    
+
     if(numFailures == 1):
         resultMessage += "1 failure. " + random.choice(failureMessages) + "\n"
     elif numFailures == 2:
         resultMessage += "2 failures. " + random.choice(failureMessages) + "\n"
     else:
         resultMessage += "No failures, phew. \n"
-    
+
     resultMessage += "```"
 
     await ctx.send(resultMessage)
